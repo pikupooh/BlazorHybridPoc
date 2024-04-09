8a9f1d76c28ef0295707e6944915bcc4a20435be
pikupooh <rohitdebnath1813@gmail.com>
2024-04-09 12:17:26 +0530

    first commit

---
+++ WinClient/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using BlazorClient.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WinClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            serviceCollection.AddSingleton<WeatherForecastService>();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+
+        }
+
+    }
+
+}
